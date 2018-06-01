f824378bfa92eafca55bf7837755241974eadf2b
Dan Patrick <dan@clouddeltav.com>
2018-06-01 11:31:07 -0400

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -39,9 +39,9 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # ---------------------------------------------------
 # Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','yourservername.mysql.database.azure.com');
+define('DBHOST','contosoosticket.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@yourservername');
+define('DBUSER','contosoadmin@contosoosticket');
 define('DBPASS','demo@pass123');
 
 # Table prefix
